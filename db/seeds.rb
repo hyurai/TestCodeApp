3fb3852262d7deb5c78c022fec19e7207ad8f7b4
hyurai <yuumanagai0504@icloud.com>
2021-09-06 05:14:10 +0000

    before_test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(
+    [
+    {
+      title: 'task 001',
+      context: 'hogehoge',
+      level: '2',
+    },
+    {
+      title: 'task 002',
+      context: 'fugafuga',
+      level: '3',
+    },
+    {
+      title: 'task 003',
+      context: 'piyopiyo',
+      level: '1',
+    },
+  ],
+    
+    
+    
+    )
